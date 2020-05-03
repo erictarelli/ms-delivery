4879311f133a61a1b99ff96a1e8f45de9dedf4a4
Eric <eric.tarelli@gmail.com>
2020-05-02 22:27:37 -0300

    Agregamos los archivos helper

---
+++ ms-delivery/Helper/BuildResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ms_delivery.Helper
+{
+    public class BuildResponse
+    {
+        public string JsonFormat(object obj)
+        {
+            return "json format";
+        }
+    }
+}
